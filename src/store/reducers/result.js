5bd6ae9b279ba23b0292eb6eeddb555fb02b10ee
Hayden Anderson <handerson@airculinaire.com>
2020-10-14 15:37:29 -0400

    basic redux implemented

---
+++ src/store/reducers/result.js
@@ -0,0 +1,22 @@
+import * as actionTypes from '../actions';
+const initialState = {
+  results: [],
+};
+
+const result = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.STORE_RESULT:
+      return {
+        ...state,
+        results: state.results.concat({ id: new Date(), value: action.result }),
+      };
+    case actionTypes.DELETE_RESULT:
+      const updatedArray = state.results.filter(
+        (element) => element.id !== action.resultElementId
+      );
+      return { ...state, results: updatedArray };
+  }
+  return state;
+};
+
+export default result;
