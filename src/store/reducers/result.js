ae0fcddd791e234d2cdd8ce35cbffe6a8876c5d7
Hayden Anderson <handerson@airculinaire.com>
2021-01-12 13:10:16 -0500

    cleanup reducer switch statements

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,20 +1,24 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
+
 const initialState = {
   results: [],
+};
+const deleteResult = (state, action) => {
+  const updatedArray = state.results.filter(
+    (element) => element.id !== action.resultElementId
+  );
+  return updateObject(state, { results: updatedArray });
 };
 
 const result = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.STORE_RESULT:
-      return {
-        ...state,
+      return updateObject(state, {
         results: state.results.concat({ id: new Date(), value: action.result }),
-      };
+      });
     case actionTypes.DELETE_RESULT:
-      const updatedArray = state.results.filter(
-        (element) => element.id !== action.resultElementId
-      );
-      return { ...state, results: updatedArray };
+      return deleteResult(state, action);
   }
   return state;
 };
